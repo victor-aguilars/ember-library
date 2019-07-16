7d69e047c40ca4159296f8b65255d94626b78384
= <=>
2019-07-16 12:37:30 -0500

    Lesson 33

---
+++ app/controllers/author/detail/new-book.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    saveBook(ev) {
+      ev.preventDefault();
+
+      let book = this.store.createRecord('book', this.model.book);
+
+      book.save()
+        .then(() => {
+          this.transitionToRoute('author.detail', this.model.author.id)
+        });
+    }
+  }
+
+});
