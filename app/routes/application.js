87687e3481f5844983202c111c52e086221f35c3
= <=>
2019-07-17 12:00:06 -0500

    Lesson 46

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,12 +3,29 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend({
   session: service('session'),
+  currentUser: service('current-user'),
 
-  init() {
+  beforeModel() {
+    this._super(...arguments);
+
+    this.loadUser();
+  },
+
+  init() {
     this._super(...arguments);
 
     this.get('session').on('invalidationSucceeded', () => {
       window.location.replace('/login');
     });
+
+    this.get('session').on('authenticationSucceeded', () => {
+      this.loadUser();
+    });
+  },
+
+  loadUser() {
+    if (this.get('session.isAuthenticated')) {
+      this.get('currentUser').load();
+    }
   }
 });
